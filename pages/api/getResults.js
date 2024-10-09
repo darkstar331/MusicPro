0a05f5fba58999137a8254d0fd766ca5f27c9fd4
darkstar331 <harman6549@gmail.com>
2024-10-09 16:36:27 -0600

    song fetch isuue

--- pages/api/getResults.js
+++ pages/api/getResults.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function handler(req, res) {
   const { query } = req.query;
 
-  const apikey = AIzaSyAnEBYcfHVXkf6LGJDhvmQzvxywEu_ZmIo
+  const apikey = process.env.YOUTUBE_API_KEY
 
   if (!query) {
     return res.status(400).json({ message: 'Query parameter is required' });
