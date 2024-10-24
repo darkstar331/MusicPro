5cea284e63e1c5d34b57be8932116303549e8b9a
darkstar331 <harman6549@gmail.com>
2024-10-23 21:11:38 -0600

    v2.0

--- app/components/Playlist.jsx
+++ app/components/Playlist.jsx
@@ -13,15 +13,15 @@ const Playlist = () => {
     };
 
     return (
-        <div className="playlist-container p-1 md:p-6 bg-[#121212] border-[#1DB954] border-l-8 rounded-lg shadow-lg">
+        <div className="playlist-container mt-16 p-1 md:p-6 rounded-lg shadow-lg">
             {playlist.length === 0 ? (
                 <p className="empty-playlist text-gray-400 text-center">No liked songs yet.</p>
             ) : (
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 overflow-y-auto max-h-[80vh] py-4 hidden-scrollbar">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 overflow-y-auto h-[28rem] py-4 hidden-scrollbar">
                     {playlist.map((song, index) => (
                         <div
                             key={index}
-                            className="cursor-pointer text-center transition-transform transform hover:scale-110 bg-[#181818] p-6 rounded-lg shadow-md hover:shadow-lg w-64 md:w-56 sm:w-48"
+                            className="cursor-pointer text-center transition-transform transform hover:scale-110 bg-[#121212] p-6 rounded-lg shadow-md hover:shadow-lg w-64 md:w-56 sm:w-48"
                             onClick={() => handleSongClick(song)}  // Add onClick handler
                         >
                             <img 
@@ -32,7 +32,7 @@ const Playlist = () => {
                             <p className="mt-2 text-lg font-semibold text-[#1db954] truncate">{song.title}</p>
                             <p className="text-sm text-gray-400 truncate">{song.artist}</p>
                             <button 
-                                className="remove-button mt-2 text-gray-500 hover:text-red-500 transition-colors" 
+                                className="remove-button mt-2 text-white hover:text-red-500 transition-colors" 
                                 onClick={(e) => {
                                     e.stopPropagation();  // Prevent the click from setting the current song
                                     handleRemove(song.videoId);
