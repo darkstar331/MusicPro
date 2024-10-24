369e3192e2fa11ad0c9e0eda1c221121fdbba6b7
darkstar331 <harman6549@gmail.com>
2024-10-24 01:17:00 -0600

    v2.0

--- app/components/MusicPlayer.jsx
+++ app/components/MusicPlayer.jsx
@@ -37,6 +37,18 @@ const MusicPlayer = () => {
 
   const [isOpen, setIsOpen] = useState(true);
   const [isFullScreen, setIsFullScreen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Detect if the device is mobile
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); // Adjust breakpoint as needed
+    };
+
+    handleResize(); // Set initial value
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   useEffect(() => {
     if (current) {
@@ -47,7 +59,7 @@ const MusicPlayer = () => {
     }
   }, [current, playlist]);
 
-  const toggleChevron = () => setIsOpen(!isOpen);
+  const toggleOpen = () => setIsOpen(!isOpen);
   const toggleFullScreen = () => setIsFullScreen(!isFullScreen);
 
   const handleLikeClick = () => {
@@ -81,167 +93,180 @@ const MusicPlayer = () => {
   const activeSong = current || dummyCurrent;
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 p-4 bg-[#121212] border-t-2 border-[#1DB954] flex flex-col md:flex-row items-center justify-between z-50">
-      {!isFullScreen && (
-        <ReactPlayer
-          ref={playerRef}
-          url={`https://www.youtube.com/watch?v=${activeSong.videoId}`}
-          playing={isPlaying}
-          onProgress={handleProgress}
-          onDuration={handleDuration}
-          onEnded={handleNext}
-          style={{ display: 'none' }}
-          key={activeSong.videoId}
-          config={{
-            youtube: {
-              playerVars: {
-                controls: 0,
-                modestbranding: 1,
-                playsinline: 1,
-                fs: 0,
-                rel: 0,
-                iv_load_policy: 3, // Hides video annotations
-              },
+    <div
+      className={`fixed bottom-0 left-0 right-0 bg-[#121212] border-t-2 border-[#1DB954] flex flex-col md:flex-row items-center justify-between z-50 transition-all duration-500 ${
+        isOpen ? 'h-auto p-4' : 'h-20 p-2'
+      }`}
+    >
+      {/* ReactPlayer is always rendered to prevent re-mounting */}
+      <ReactPlayer
+        ref={playerRef}
+        url={`https://www.youtube.com/watch?v=${activeSong.videoId}`}
+        playing={isPlaying}
+        onProgress={handleProgress}
+        onDuration={handleDuration}
+        onEnded={handleNext}
+        style={{ display: 'none' }}
+        config={{
+          youtube: {
+            playerVars: {
+              controls: 0,
+              modestbranding: 1,
+              playsinline: 1,
+              fs: 0,
+              rel: 0,
+              iv_load_policy: 3,
             },
-          }}
-        />
-      )}
+          },
+        }}
+      />
 
-      {!isFullScreen && (
-        <div className="flex items-center w-full h-12 md:h-auto md:w-[25%] gap-3 mb-1 md:mb-0">
-          {isOpen ? (
-            <>
-              <img
-                src={activeSong.thumbnail}
-                alt="Song Thumbnail"
-                className="w-16 h-16 rounded-lg"
-              />
-              <div className="flex flex-col justify-center text-white truncate">
-                <span className="font-semibold text-sm md:text-md">
-                  {activeSong.title}
-                </span>
-              </div>
-              <button
-                onClick={handleLikeClick}
-                className="ml-3 focus:outline-none"
-              >
-                {isLiked ? (
-                  <FaHeart className="text-red-700 text-2xl transition-colors duration-200" />
-                ) : (
-                  <FaRegHeart className="text-gray-300 text-2xl hover:text-[#1ed760] transition-colors duration-200" />
-                )}
-              </button>
-            </>
+      {/* Left Section (Thumbnail and Song Info) */}
+      <div className="flex items-center w-full md:w-1/3 gap-3">
+        <img
+          src={activeSong.thumbnail}
+          alt="Song Thumbnail"
+          className="w-16 h-16 rounded-lg"
+        />
+        <div className="flex flex-col justify-center text-white truncate">
+          <span className="font-semibold text-sm md:text-md">
+            {activeSong.title}
+          </span>
+        </div>
+        <button onClick={handleLikeClick} className="ml-3 focus:outline-none">
+          {isLiked ? (
+            <FaHeart className="text-red-700 absolute top-9 right-16 text-2xl transition-colors duration-200" />
           ) : (
-            <div className="flex space-x-6">
-              <div>
-                <img
-                  src={activeSong.thumbnail}
-                  alt="Song Thumbnail"
-                  className="w-16 h-16 rounded-lg"
-                />
-              </div>
-              <div className="flex flex-col justify-center text-white truncate">
-                <span className="font-semibold text-sm md:text-md w-[75vw]">
-                  {activeSong.title}
-                </span>
-              </div>
-            </div>
+            <FaRegHeart className="text-gray-300 absolute top-9 right-16 text-2xl hover:text-[#1ed760] transition-colors duration-200" />
           )}
+        </button>
+
+        {/* Toggle Button */}
+        <button
+          className="ml-auto text-white focus:outline-none md:hidden"
+          onClick={toggleOpen}
+        >
+          {isOpen ? (
+            <FaChevronDown className="h-6 w-6" />
+          ) : (
+            <FaChevronUp className="h-6 w-6" />
+          )}
+        </button>
+      </div>
+
+      {/* Center Section (Controls) */}
+      {isOpen && (
+        <div className="flex justify-center items-center gap-4 w-full md:w-1/3 mt-4 md:mt-0">
+          <button
+            className="text-white hover:text-[#1ed760] transition-colors duration-200 focus:outline-none"
+            onClick={skipPrevious}
+          >
+            <FaStepBackward style={{ fontSize: '30px' }} />
+          </button>
+          <button
+            className="text-white hover:text-[#1ed760] transition-colors duration-200 focus:outline-none"
+            onClick={togglePlayPause}
+          >
+            {isPlaying ? (
+              <FaPause
+                className="text-orange-400"
+                style={{ fontSize: '40px' }}
+              />
+            ) : (
+              <FaPlay
+                className="text-orange-400"
+                style={{ fontSize: '40px' }}
+              />
+            )}
+          </button>
+          <button
+            className="text-white hover:text-[#1ed760] transition-colors duration-200 focus:outline-none"
+            onClick={handleNext}
+          >
+            <FaStepForward style={{ fontSize: '30px' }} />
+          </button>
         </div>
       )}
 
-      {isOpen && !isFullScreen && (
-        <>
-          <div className="flex justify-center items-center gap-4 w-full md:w-[30%] mb-2 md:mb-0">
-            <button
-              className="text-white hover:text-[#1ed760] transition-colors duration-200 focus:outline-none"
-              onClick={skipPrevious}
-            >
-              <FaStepBackward style={{ fontSize: '30px' }} />
-            </button>
-            <button
-              className="text-white hover:text-[#1ed760] transition-colors duration-200 focus:outline-none"
-              onClick={togglePlayPause}
-            >
-              {isPlaying ? (
-                <FaPause
-                  className="text-orange-400"
-                  style={{ fontSize: '40px' }}
-                />
-              ) : (
-                <FaPlay
-                  className="text-orange-400"
-                  style={{ fontSize: '40px' }}
-                />
-              )}
-            </button>
-            <button
-              className="text-white hover:text-[#1ed760] transition-colors duration-200 focus:outline-none"
-              onClick={handleNext}
-            >
-              <FaStepForward style={{ fontSize: '30px' }} />
-            </button>
+      {/* Right Section (Seek Bar) */}
+      {isOpen && (
+        <div className="flex flex-col w-full md:w-1/3 items-center mt-4 md:mt-0">
+          <input
+            type="range"
+            min={0}
+            max={1}
+            step="any"
+            value={played}
+            onChange={handleSeekChange}
+            className={`w-full h-1 rounded-lg appearance-none cursor-pointer focus:outline-none ${
+              isPlaying ? 'bg-[#1DB954]' : 'bg-gray-600'
+            } transition-colors duration-200`}
+          />
+          <div className="time text-white text-xs mt-1 flex justify-between w-full px-2">
+            <span>{formatTime(played * duration)}</span>
+            <span>{formatTime(duration)}</span>
           </div>
-
-          <div className="flex flex-col w-full md:w-[25%] items-center">
-            <input
-              type="range"
-              min={0}
-              max={1}
-              step="any"
-              value={played}
-              onChange={handleSeekChange}
-              className={`w-full h-1 rounded-lg appearance-none cursor-pointer focus:outline-none ${
-                isPlaying ? 'bg-[#1DB954]' : 'bg-gray-600'
-              } transition-colors duration-200`}
-            />
-            <div className="time text-white text-xs mt-1 flex justify-between w-full px-2">
-              <span>{formatTime(played * duration)}</span>
-              <span>{formatTime(duration)}</span>
-            </div>
-          </div>
-        </>
+        </div>
       )}
 
+      {/* Full Screen Toggle */}
       <button
-        className="absolute -top-6 right-2 text-white"
+        className="absolute hidden md:block -top-6 right-2 text-white focus:outline-none"
         onClick={toggleFullScreen}
       >
         <FaChevronUp className="h-6 w-12" />
       </button>
 
+      {/* Full Screen Mode */}
       {isFullScreen && (
         <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black bg-opacity-90">
-          <div className="relative">
-            <ReactPlayer
-              ref={playerRef}
-              url={`https://www.youtube.com/watch?v=${activeSong.videoId}`}
-              playing={isPlaying}
-              onProgress={handleProgress}
-              onDuration={handleDuration}
-              onEnded={handleNext}
-              width="80vw"
-              height="45vw"
-              config={{
-                youtube: {
-                  playerVars: {
-                    controls: 0,
-                    modestbranding: 1,
-                    playsinline: 1,
-                    fs: 0,
-                    rel: 0,
-                    iv_load_policy: 3, // Hides video annotations
+          {/* Conditionally render ReactPlayer in full-screen mode based on device */}
+          {!isMobile && (
+            <div className="relative">
+              <ReactPlayer
+                ref={playerRef}
+                url={`https://www.youtube.com/watch?v=${activeSong.videoId}`}
+                playing={isPlaying}
+                onProgress={handleProgress}
+                onDuration={handleDuration}
+                onEnded={handleNext}
+                width="80vw"
+                height="45vw"
+                config={{
+                  youtube: {
+                    playerVars: {
+                      controls: 0,
+                      modestbranding: 1,
+                      playsinline: 1,
+                      fs: 0,
+                      rel: 0,
+                      iv_load_policy: 3,
+                    },
                   },
-                },
-              }}
-            />
-            {/* Overlay div to cover YouTube overlays */}
-            <div
-              className="absolute top-0 left-0 w-full h-full"
-              style={{ backgroundColor: 'transparent', pointerEvents: 'none' }}
-            ></div>
-          </div>
+                }}
+              />
+              {/* Overlay to cover YouTube overlays */}
+              <div
+                className="absolute top-0 left-0 w-full h-full"
+                style={{
+                  backgroundColor: 'transparent',
+                  pointerEvents: 'none',
+                }}
+              ></div>
+            </div>
+          )}
+
+          {/* Display alternative content on mobile */}
+          {isMobile && (
+            <div className="flex flex-col items-center">
+              <img
+                src={activeSong.thumbnail}
+                alt="Song Thumbnail"
+                className="w-80 h-80 rounded-lg"
+              />
+            </div>
+          )}
+
           <div className="text-white text-lg mt-4">{activeSong.title}</div>
           <div className="flex justify-center items-center gap-4 mt-4">
             <button
@@ -267,8 +292,9 @@ const MusicPlayer = () => {
               <FaStepForward style={{ fontSize: '30px' }} />
             </button>
           </div>
+          {/* Close Full Screen Button */}
           <button
-            className="absolute top-4 right-4 text-white"
+            className="absolute top-4 right-4 text-white focus:outline-none"
             onClick={toggleFullScreen}
           >
             <FaChevronDown className="h-12 w-20" />
