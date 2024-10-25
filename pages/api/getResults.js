6a96b76636fee8353008dbcfae79a1b7228169ca
darkstar331 <harman6549@gmail.com>
2024-10-24 20:40:11 -0600

    api update

--- pages/api/getResults.js
+++ pages/api/getResults.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function handler(req, res) {
   const { query } = req.query;
 
-  const apikey = "AIzaSyDUD_qplFxQ9AT8M4zhvpUHSYHjhUmDF28"
+  const apikey = "AIzaSyAnEBYcfHVXkf6LGJDhvmQzvxywEu_ZmIo"
 
   if (!query) {
     return res.status(400).json({ message: 'Query parameter is required' });
