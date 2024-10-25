b0d6ddad6bf55e51983eef3a40f2ba0c937ca8aa
darkstar331 <harman6549@gmail.com>
2024-10-24 18:18:35 -0600

    v2.0

--- app/components/Playlist.jsx
+++ app/components/Playlist.jsx
@@ -1,7 +1,7 @@
 import { useSession } from 'next-auth/react';
 import React from 'react';
 import { useMusicPlayer } from '../context/MusicPlayerContext';
-import { FaTrash, FaMusic, FaPlay } from 'react-icons/fa';
+import { FaTrash, FaPlay } from 'react-icons/fa';
 
 const Playlist = () => {
     const { playlist, setCurrent, handleRemove } = useMusicPlayer();
@@ -12,43 +12,33 @@ const Playlist = () => {
     };
 
     return (
-        <div className="playlist-container mt-16 p-4 md:p-10 rounded-lg shadow-xl text-white bg-transparent">
+        <div className="mt-16 p-4 md:p-10 text-white h-screen overflow-y-auto">
             {playlist.length === 0 ? (
-                <p className="empty-playlist text-gray-400 text-center text-lg py-20">
+                <p className="text-gray-400 text-center text-lg py-20">
                     No liked songs yet. Start adding some!
                 </p>
             ) : (
-                <div className="grid grid-cols-1 m-2 px-5  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 overflow-y-auto h-[28rem] py-4 hidden-scrollbar">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 py-4">
                     {playlist.map((song, index) => (
                         <div
                             key={index}
-                            className="group cursor-pointer text-center transition-transform transform hover:scale-105 bg-[#1a1a1a] p-6 rounded-lg shadow-lg hover:shadow-2xl w-64 md:w-56 sm:w-48 relative"
-                            onClick={() => handleSongClick(song)}  // Add onClick handler
+                            className="group cursor-pointer transition-transform transform hover:scale-105 user-select-none w-full max-w-[300px] mx-auto border border-[#ffffff22] bg-[#282c34] bg-gradient-to-b from-[#282c34] to-[rgba(17,0,32,0.5)] shadow-lg rounded-lg backdrop-blur-md overflow-hidden p-4"
+                            onClick={() => handleSongClick(song)}
                         >
-                            <div className="relative flex rounded-full items-center justify-center w-full h-48 bg-emerald-800  shadow-md md:h-48 sm:h-40">
-                                <FaMusic className="text-6xl text-gray-400 group-hover:text-gray-200" />
-                                <div className="absolute bottom-2 right-2 bg-black bg-opacity-60 rounded-full p-1 group-hover:bg-opacity-80">
-                                    <button 
-                                        className="remove-button text-white hover:text-red-500 transition-colors" 
-                                        onClick={(e) => {
-                                            e.stopPropagation();  // Prevent the click from setting the current song
-                                            handleRemove(song.videoId);
-                                        }}
-                                        aria-label="Remove song"
-                                    >
-                                        <FaTrash className="remove-icon w-5 h-5" />
-                                    </button>
-                                </div>
-                            </div>
-                            <div className="mt-4">
-                                <p className="text-lg font-bold text-white truncate group-hover:text-[#1db954] transition-colors">
+                            <div className="flex flex-col">
+                                <img
+                                    className='rounded-md w-full h-[250px] object-cover mb-4'
+                                    src="https://images.unsplash.com/photo-1621075160523-b936ad96132a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                                    alt="NFT"
+                                />
+                                <h2 className="text-lg font-bold text-white truncate group-hover:text-[#1db954] transition-colors mb-2">
                                     {song.title}
-                                </p>
-                                <p className="text-sm text-gray-400 truncate mt-1">
+                                </h2>
+                                <p className='text-gray-400 truncate mb-4'>
                                     {song.artist}
                                 </p>
-                                <button 
-                                    className="play-button mt-4 text-sm px-5 py-2 bg-[#1db954] text-white rounded-full hover:bg-[#17a54d] transition-all ease-in-out duration-200 transform hover:-translate-y-1"
+                                <button
+                                    className="w-full text-sm px-5 py-2 bg-[#1db954] text-white rounded-full hover:bg-[#17a54d] transition-all ease-in-out duration-200 transform hover:-translate-y-1"
                                     onClick={(e) => {
                                         e.stopPropagation();  // Prevent the click from setting the current song
                                         handleSongClick(song);
@@ -66,3 +56,7 @@ const Playlist = () => {
 };
 
 export default Playlist;
+
+
+
+
